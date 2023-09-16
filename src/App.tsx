3f7717d191841c116a0c1ce2ee1f2a91bd1b5dbc
ppegu <ppegu@cisco.com>
2023-09-16 17:35:42 +0530

    status bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Navigation from './navigation';
 import { View } from './theme/View';
 import { BACKGROUND_COLOR } from './utils/color.util';
+import CustomStatusbar from './components/Statusbar';
 
 export default function App() {
   return (
     <View flex={1} backgroundColor={BACKGROUND_COLOR}>
+      <CustomStatusbar />
       <Navigation />
     </View>
   );
---
+++ src/components/Statusbar.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+export default function CustomStatusbar() {
+  return <StatusBar />;
+}
